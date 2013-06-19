5eaf58e17c760007fadf7868a305a8e5bb98a3d5
hammy <hammy@hambox-9000>
2013-06-19 05:32:22 -0700

    Moved shared T-Tree functions into a parent class. Specialization can now be performed with little maintenance.

--- include/containers/tTree.h
+++ include/containers/tTree.h
@@ -22,114 +22,66 @@
 namespace hamLibs {
 namespace containers {
 
+enum node_dir : int {
+    LEFT_NODE   = 0,
+    MID_NODE    = 1,
+    RIGHT_NODE  = 2,
+    MAX_NODE    = 3
+};
+
+struct bitmask {
+    char b1 : 1;
+    char b2 : 1;
+    char b3 : 1;
+    char b4 : 1;
+    char b5 : 1;
+    char b6 : 1;
+    char b7 : 1;
+    char b8 : 1;
+
+    char operator[] ( unsigned i ) const {
+        switch( i ) {
+            case 0 : return b1;
+            case 1 : return b2;
+            case 2 : return b3;
+            case 3 : return b4;
+            case 4 : return b5;
+            case 5 : return b6;
+            case 6 : return b7;
+            case 7 : return b8;
+        }
+        return 0; // Avoiding a compiler error
+    }
+};
+
+template <typename data_t>
+struct node {
+    bitmask bits = { 0,0,0,0,0,0,0,0 };
+    data_t* data = nullptr;
+    node*   subNodes = {nullptr};
+
+    ~node() {
+        delete data;
+        delete [] subNodes;
+        data = nullptr;
+        subNodes = nullptr;
+    }
+};
+
 /******************************************************************************
  *  Tri-Tree Structure Setup
 ******************************************************************************/
-class treeTrunk {
-    public:
-        enum node_dir : int {
-            LEFT_NODE   = 0,
-            MID_NODE    = 1,
-            RIGHT_NODE  = 2,
-            MAX_NODE    = 3
-        };
-
-        struct bitmask {
-            char b1 : 1;
-            char b2 : 1;
-            char b3 : 1;
-            char b4 : 1;
-            char b5 : 1;
-            char b6 : 1;
-            char b7 : 1;
-            char b8 : 1;
-
-            char operator[] ( unsigned i ) const {
-                switch( i ) {
-                    case 0 : return b1;
-                    case 1 : return b2;
-                    case 2 : return b3;
-                    case 3 : return b4;
-                    case 4 : return b5;
-                    case 5 : return b6;
-                    case 6 : return b7;
-                    case 7 : return b8;
-                }
-                return 0; // Avoiding a compiler error
-            }
-        };
-
-        virtual ~treeTrunk() = 0;
-};
-
-inline treeTrunk::~treeTrunk() {}
-
-/******************************************************************************
- *  Tri-Tree Structure 
-******************************************************************************/
 template <typename key_t, typename data_t>
-class triTree : private treeTrunk {
+class tTreeBase {
     
-    struct node {
-        bitmask bits = { 0,0,0,0,0,0,0,0 };
-        data_t* data = nullptr;
-        node*   subNodes = {nullptr};
-        
-        ~node() {
-            delete data;
-            delete [] subNodes;
-            data = nullptr;
-            subNodes = nullptr;
-        }
-    };
-    
-    node* iterate( const key_t& k, bool createNodes ) {
-        node*           nodeIter    = const_cast< node* >( &head );
-        node_dir        dir;
-
-        for ( unsigned i = 0; i < sizeof( key_t ); ++i ) {
-            // iterate through each set of bits within the key
-            const bitmask*  bitIter = reinterpret_cast< const bitmask* >( &k ) + i;
-
-            for ( unsigned j = 0; j < HL_BITS_PER_BYTE; ++j ) {
-                // compare the bits of each byte in k
-                if ( bitIter->operator[](j) < nodeIter->bits[j] ) {
-                    dir = LEFT_NODE;
-                }
-                else if ( bitIter->operator[](j) > nodeIter->bits[j] ) {
-                    dir = RIGHT_NODE;
-                }
-                else {
-                    dir = MID_NODE;
-                }
-
-                // check to see if a new node needs to be made
-                if ( !nodeIter->subNodes ) {
-                    if ( createNodes ) {
-                        // create and initialize the upcoming sub node
-                        nodeIter->subNodes = new node[ MAX_NODE ];
-                        nodeIter->subNodes[ dir ].bits = *bitIter;
-                    }
-                    else {
-                        return nullptr;
-                    }
-                }
-
-                // move to the next node
-                nodeIter = &(nodeIter->subNodes[ dir ]);
-            }
-        }
-        return nodeIter;
-    }
-    
-    private:
-        node head;
+    protected:
+        node<data_t> head;
         int numNodes = 0;
         
-        
     public:
-        triTree         ()      {}
-        ~triTree        ()      {}
+        virtual ~tTreeBase() {}
+        
+        virtual node<data_t>* iterate( const void* key, bool createSubNodes ) = 0;
         
         // STL-Map behavior
         data_t&         operator [] ( const key_t& k );
@@ -140,15 +92,14 @@ class triTree : private treeTrunk {
         const data_t*   getData     ( const key_t& k );
         int             size        () const { return numNodes; }
         void            clear       () { delete [] head.subNodes; delete head.data; }
-
-}; // end triTree class
+};
 
 /*
  * Tri-Tree -- Array Subscript operators
  */
 template <typename key_t, typename data_t>
-data_t& triTree<key_t, data_t>::operator []( const key_t& k ) {
-    node* iter = iterate( k, true );
+data_t& tTreeBase<key_t, data_t>::operator []( const key_t& k ) {
+    node<data_t>* iter = iterate( &k, true );
     
     if ( !iter->data ) {
         iter->data = new data_t();
@@ -163,8 +114,8 @@ data_t& triTree<key_t, data_t>::operator []( const key_t& k ) {
  * Push a data element to the tree using a key
  */
 template <typename key_t, typename data_t>
-void triTree<key_t, data_t>::push( const key_t& k, const data_t& d ) {
-    node* iter = iterate( k, true );
+void tTreeBase<key_t, data_t>::push( const key_t& k, const data_t& d ) {
+    node<data_t>* iter = iterate( &k, true );
     
     if ( !iter->data ) {
         iter->data = new data_t( d );
@@ -180,8 +131,8 @@ void triTree<key_t, data_t>::push( const key_t& k, const data_t& d ) {
  * Remove whichever element lies at the key
  */
 template <typename key_t, typename data_t>
-void triTree<key_t, data_t>::pop( const key_t& k ) {
-    node* iter = iterate( k, false );
+void tTreeBase<key_t, data_t>::pop( const key_t& k ) {
+    node<data_t>* iter = iterate( &k, false );
 
     if ( !iter || iter->data )
         return;
@@ -196,8 +147,8 @@ void triTree<key_t, data_t>::pop( const key_t& k ) {
  * Return true if there is a data element at the key
  */
 template <typename key_t, typename data_t>
-bool triTree<key_t, data_t>::hasData( const key_t& k ) const {
-    node* iter = iterate( k, false );
+bool tTreeBase<key_t, data_t>::hasData( const key_t& k ) const {
+    node<data_t>* iter = iterate( &k, false );
 
     return iter && ( iter->data != nullptr );
 }
@@ -208,8 +159,8 @@ bool triTree<key_t, data_t>::hasData( const key_t& k ) const {
  * Returns a nullptr if no data exists
  */
 template <typename key_t, typename data_t>
-const data_t* triTree<key_t, data_t>::getData( const key_t& k ) {
-    node* iter = iterate( k, false );
+const data_t* tTreeBase<key_t, data_t>::getData( const key_t& k ) {
+    node<data_t>* iter = iterate( &k, false );
 
     if ( !iter )
         return nullptr;
@@ -218,148 +169,117 @@ const data_t* triTree<key_t, data_t>::getData( const key_t& k ) {
 }
 
 /******************************************************************************
+ *  Tri-Tree Structure 
+******************************************************************************/
+template <typename key_t, typename data_t>
+class tTree final : public tTreeBase<key_t, data_t> {
+    
+    private:
+        node<data_t>* iterate( const void* k, bool createNodes );
+    
+    public:
+        tTree         ()      {}
+        ~tTree        ()      {}
+
+}; // end tTree class
+    
+template <typename key_t, typename data_t>
+node<data_t>* tTree<key_t, data_t>::iterate( const void* k, bool createNodes ) {
+
+    node<data_t>* nodeIter = const_cast< node<data_t>* >( &this->head );
+    node_dir dir;
+
+    for ( unsigned i = 0; i < sizeof( key_t ); ++i ) {
+        // iterate through each set of bits within the key
+        const bitmask*  bitIter = reinterpret_cast< const bitmask* >( k ) + i;
+
+        for ( unsigned j = 0; j < HL_BITS_PER_BYTE; ++j ) {
+            // compare the bits of each byte in k
+            if ( bitIter->operator[](j) < nodeIter->bits[j] ) {
+                dir = LEFT_NODE;
+            }
+            else if ( bitIter->operator[](j) > nodeIter->bits[j] ) {
+                dir = RIGHT_NODE;
+            }
+            else {
+                dir = MID_NODE;
+            }
+
+            // check to see if a new node needs to be made
+            if ( !nodeIter->subNodes ) {
+                if ( createNodes ) {
+                    // create and initialize the upcoming sub node
+                    nodeIter->subNodes = new node<data_t>[ MAX_NODE ];
+                    nodeIter->subNodes[ dir ].bits = *bitIter;
+                }
+                else {
+                    return nullptr;
+                }
+            }
+
+            // move to the next node
+            nodeIter = &(nodeIter->subNodes[ dir ]);
+        }
+    }
+    return nodeIter;
+}
+
+
+/******************************************************************************
  *  Tri-Tree Structure -- C-string partial specialization
 ******************************************************************************/
 template <typename data_t>
-class triTree<const char*, data_t> : private treeTrunk  {
-    
-    struct node {
-        bitmask bits = { 0,0,0,0,0,0,0,0 };
-        data_t* data = nullptr;
-        node*   subNodes = {nullptr};
-        
-        ~node() {
-            delete data;
-            delete [] subNodes;
-            data = nullptr;
-            subNodes = nullptr;
-        }
-    };
-    
-    node* iterate( const char* str, bool createNodes ) {
-        node*           nodeIter    = const_cast< node* >( &head );
-        node_dir        dir;
-
-        for ( unsigned i = 0; str[ i ] != '\0'; ++i ) {
-            // iterate through each set of bits within the key
-            const bitmask*  bitIter = reinterpret_cast< const bitmask* >( str+i );
-
-            for ( unsigned j = 0; j < HL_BITS_PER_BYTE; ++j ) {
-                // compare the bits of each byte in k
-                if ( bitIter->operator[](j) < nodeIter->bits[j] ) {
-                    dir = LEFT_NODE;
-                }
-                else if ( bitIter->operator[](j) > nodeIter->bits[j] ) {
-                    dir = RIGHT_NODE;
-                }
-                else {
-                    dir = MID_NODE;
-                }
-
-                // check to see if a new node needs to be made
-                if ( !nodeIter->subNodes ) {
-                    if ( createNodes ) {
-                        // create and initialize the upcoming sub node
-                        nodeIter->subNodes = new node[ MAX_NODE ];
-                        nodeIter->subNodes[ dir ].bits = *bitIter;
-                    }
-                    else {
-                        return nullptr;
-                    }
-                }
-
-                // move to the next node
-                nodeIter = &(nodeIter->subNodes[ dir ]);
-            }
-        }
-        return nodeIter;
-    }
+class tTree<const char*, data_t> final : public tTreeBase<const char*, data_t>  {
     
     private:
-        node head;
-        int numNodes = 0;
-        
+        node<data_t>* iterate( const void* str, bool createNodes );
         
     public:
-        triTree         ()      {}
-        ~triTree        ()      {}
+        tTree         ()      {}
+        ~tTree        ()      {}
         
-        // STL-Map behavior
-//        data_t&         operator [] ( const char* str );
-//        
-//        void            push        ( const char* str, const data_t& d );
-//        void            pop         ( const char* str );
-//        bool            hasData     ( const char* str ) const;
-//        const data_t*   getData     ( const char* str );
-//        int             size        () const { return numNodes; }
-//        void            clear       () { delete [] head.subNodes; delete head.data; }
-        
-        data_t& operator []( const char* str ) {
-            node* iter = iterate( str, true );
+}; // end tTree class, string specialization
+    
+template <typename data_t>
+node<data_t>* tTree<const char*, data_t>::iterate( const void* str, bool createNodes ) {
+    node<data_t>* nodeIter = const_cast< node<data_t>* >( &this->head );
+    node_dir dir;
+    const char* s = (const char*)str;
 
-            if ( !iter->data ) {
-                iter->data = new data_t();
-                ++numNodes;
+    for ( unsigned i = 0; s[ i ] != '\0'; ++i ) {
+        // iterate through each set of bits within the key
+        const bitmask*  bitIter = reinterpret_cast< const bitmask* >( s+i );
+
+        for ( unsigned j = 0; j < HL_BITS_PER_BYTE; ++j ) {
+            // compare the bits of each byte in k
+            if ( bitIter->operator[](j) < nodeIter->bits[j] ) {
+                dir = LEFT_NODE;
             }
-
-            return *iter->data;
-        }
-
-        /*
-         * Tri-Tree -- Push
-         * Push a data element to the tree using a key
-         */
-        void push( const key_t& k, const data_t& d ) {
-            node* iter = iterate( k, true );
-
-            if ( !iter->data ) {
-                iter->data = new data_t( d );
-                ++numNodes;
+            else if ( bitIter->operator[](j) > nodeIter->bits[j] ) {
+                dir = RIGHT_NODE;
             }
             else {
-                *iter->data = d;
+                dir = MID_NODE;
             }
+
+            // check to see if a new node needs to be made
+            if ( !nodeIter->subNodes ) {
+                if ( createNodes ) {
+                    // create and initialize the upcoming sub node
+                    nodeIter->subNodes = new node<data_t>[ MAX_NODE ];
+                    nodeIter->subNodes[ dir ].bits = *bitIter;
+                }
+                else {
+                    return nullptr;
+                }
+            }
+
+            // move to the next node
+            nodeIter = &(nodeIter->subNodes[ dir ]);
         }
-
-        /*
-         * Tri-Tree -- Pop
-         * Remove whichever element lies at the key
-         */
-        void pop( const key_t& k ) {
-            node* iter = iterate( k, false );
-
-            if ( !iter || iter->data )
-                return;
-
-            delete iter->data;
-            iter->data = nullptr;
-            --numNodes;
-        }
-
-        /*
-         * Tri-Tree -- Has Data
-         * Return true if there is a data element at the key
-         */
-        bool hasData( const char* str ) const {
-            node* iter = iterate( str, false );
-
-            return iter && ( iter->data != nullptr );
-        }
-
-        /*
-         * Tri-Tree -- Push
-         * Return a pointer to the data that lies at a key
-         * Returns a nullptr if no data exists
-         */
-        const data_t* getData( const char* str ) {
-            node* iter = iterate( str, false );
-
-            if ( !iter )
-                return nullptr;
-
-            return iter->data;
-        }
-}; // end triTree class, string specialization
+    }
+    return nodeIter;
+}
 
 } // end containers namespace
 } // end hamLibs namespace

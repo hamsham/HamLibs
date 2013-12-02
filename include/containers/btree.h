af23457ca86c2e2291f91c7e95d7e117b24cc292
hammy <hammy@192.168.1.3>
2013-12-01 18:14:23 -0800

    Added copy/move operators and constructors for type-safety.

--- include/containers/btree.h
+++ include/containers/btree.h
@@ -8,24 +8,128 @@
 #ifndef __HL_B_TREE_H__
 #define __HL_B_TREE_H__
 
+#include <iostream>
+#include <new>
 #include "../utils/bits.h"
 
 namespace hamLibs {
 namespace containers {
 
-template <typename data_t>
-struct bTreeNode {
-    data_t*     data        = nullptr;
-    bTreeNode*  subNodes    = nullptr;
+enum bnode_dir : unsigned {
+    BNODE_LEFT   = 0,
+    BNODE_RIGHT  = 1,
+    BNODE_MAX
+};
 
-    ~bTreeNode() {
-        delete data;
-        delete [] subNodes;
+/**
+ * Binary Tree Node
+ */
+template <typename data_t>
+class bTreeNode {
+    private:
+        template <typename, typename>
+        friend class bTree;
+    
+        data_t*     data        = nullptr;
+        bTreeNode*  subNodes    = nullptr;
         
+    public:
+        
+        constexpr bTreeNode();
+        bTreeNode( const bTreeNode& btn );
+        bTreeNode( bTreeNode&& btn );
+
+        ~bTreeNode();
+        
+        bTreeNode& operator=( const bTreeNode& btn );
+        bTreeNode& operator=( bTreeNode&& btn );
+};
+
+/**
+ * B-Tree Node Constructor
+ */
+template <typename data_t>
+constexpr bTreeNode<data_t>::bTreeNode() {}
+
+/**
+ * B-Tree Node Copy Constructor
+ */
+template <typename data_t>
+bTreeNode<data_t>::bTreeNode( const bTreeNode& btn ) :
+    data{
+        btn.data != nullptr
+            ? new(std::nothrow) data_t(*btn.data)
+            : nullptr
+    },
+    subNodes{nullptr}
+{
+    if (btn.subNodes != nullptr) {
+        subNodes = new(std::nothrow) bTreeNode[ BNODE_MAX ];
+        subNodes[ BNODE_LEFT ] = btn.subNodes[ BNODE_LEFT ];
+        subNodes[ BNODE_RIGHT ] = btn.subNodes[ BNODE_RIGHT ];
+    }
+}
+
+/**
+ * B-Tree Node Move Constructor
+ */
+template <typename data_t>
+bTreeNode<data_t>::bTreeNode( bTreeNode&& btn ) :
+    data{ btn.data },
+    subNodes{ btn.subNodes }
+{
+    btn.data = nullptr;
+    btn.subNodes = nullptr;
+}
+
+/**
+ * B-Tree Node Destructor
+ */
+template <typename data_t>
+bTreeNode<data_t>::~bTreeNode() {
+    delete data;
+    data = nullptr;
+    
+    delete [] subNodes;
+    subNodes = nullptr;
+}
+
+/**
+ * B-Tree Node Copy Operator
+ */
+template <typename data_t>
+bTreeNode<data_t>& bTreeNode<data_t>::operator=( const bTreeNode& btn ) {
+    delete data;
+    if (btn.data) {
+        data = new(std::nothrow) data_t;
+        *data = *btn.data;
+    }
+    else {
         data = nullptr;
+    }
+    
+    delete subNodes;
+    if (btn.subNodes) {
+        subNodes = new(std::nothrow) bTreeNode[ BNODE_MAX ];
+        subNodes[ BNODE_LEFT ] = btn.subNodes[ BNODE_LEFT ];
+        subNodes[ BNODE_RIGHT ] = btn.subNodes[ BNODE_RIGHT ];
+    }
+    else {
         subNodes = nullptr;
     }
-};
+}
+
+/**
+ * B-Tree Node Move Operator
+ */
+template <typename data_t>
+bTreeNode<data_t>& bTreeNode<data_t>::operator=( bTreeNode&& btn ) {
+    data = btn.data;
+    btn.data = nullptr;
+    
+    subNodes = btn.subNodes;
+    btn.subNodes = nullptr;
+}
 
 /******************************************************************************
  *  Binary-Tree Structure Setup
@@ -35,21 +139,22 @@ struct bTreeNode {
 template <typename key_t, typename data_t>
 class bTree {
 
-    enum node_dir : unsigned {
-        BNODE_LEFT   = 0,
-        BNODE_RIGHT  = 1,
-        BNODE_MAX
-    };
-
     protected:
-        bTreeNode<data_t>   head;
+        bTreeNode<data_t>   head = bTreeNode<data_t>();
         unsigned            numNodes = 0;
         
     private:
         bTreeNode<data_t>* iterate( const key_t* k, bool createNodes );
         
     public:
-        ~bTree() {}
+        constexpr bTree ();
+        bTree           ( const bTree& );
+        bTree           ( bTree&& );
+        
+        ~bTree          () {}
+        
+        bTree&          operator=   ( const bTree& );
+        bTree&          operator=   ( bTree&& );
         
         // STL-Map behavior
         data_t&         operator [] ( const key_t& k );
@@ -60,12 +165,81 @@ class bTree {
         const data_t*   getData     ( const key_t& k );
         unsigned        size        () const { return numNodes; }
         void            clear       ();
+        
+        /*
+         * Iterator
+         */
+        class iterator {
+            private:
+            protected:
+                iterator();
+                iterator( const iterator& );
+                iterator( iterator&& );
+                
+                ~iterator() {}
+                
+                iterator& operator=( const iterator& );
+                iterator& operator=( iterator&& );
+        };
 };
 
+/*
+ * Binary Tree -- Constructor
+ */
+template <typename key_t, typename data_t>
+constexpr bTree<key_t, data_t>::bTree() :
+    head{},
+    numNodes{0}
+{}
+
+/*
+ * Binary Tree -- Copy Constructor
+ */
+template <typename key_t, typename data_t>
+bTree<key_t, data_t>::bTree( const bTree& bt ) :
+    head{ bt.head },
+    numNodes{ bt.numNodes }
+{}
+
+/*
+ * Binary Tree -- Move Constructor
+ */
+template <typename key_t, typename data_t>
+bTree<key_t, data_t>::bTree( bTree&& bt ) {
+    head.subNodes = bt.head.subNodes;
+    bt.head.subNodes = nullptr;
+    
+    numNodes = bt.numNodes;
+    bt.numNodes = 0;
+}
+
+/*
+ * Binary Tree -- Copy Operator
+ */
+template <typename key_t, typename data_t>
+bTree<key_t, data_t>& bTree<key_t, data_t>::operator =( const bTree& bt ) {
+    head = bt.head;
+    numNodes = bt.numNodes;
+    return *this;
+}
+
+/*
+ * Binary Tree -- Move Operator
+ */
+template <typename key_t, typename data_t>
+bTree<key_t, data_t>& bTree<key_t, data_t>::operator =( bTree&& bt ) {
+    head.subNodes = bt.head.subNodes;
+    bt.head.subNodes = nullptr;
+    
+    numNodes = bt.numNodes;
+    bt.numNodes = 0;
+    
+    return *this;
+}
 
 /*
  * Binary-Tree -- Element iteration
- */ 
+ */
 template <typename key_t, typename data_t>
 bTreeNode<data_t>* bTree<key_t, data_t>::iterate( const key_t* k, bool createNodes ) {
     
--- tests/tests/bTreeTest.cpp
+++ tests/tests/bTreeTest.cpp
@@ -7,12 +7,16 @@
 #include <chrono>
 #include <limits>
 #include <thread>
+#include <utility>
 
 #include "containers/btree.h"
 
 #define NUM_ITERATIONS 4096
 #define NUM_TESTS 4096
 
+const char* testKey1 = "Hello World!";
+const char* testKey2 = "Supercalifragilisticexpialidocious!";
+const char* testKey3 = "One, Two, Three";
 const char* loremIpsum = R"***(Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum purus vel libero scelerisque, eu tincidunt nibh semper. Mauris nibh lectus, ullamcorper et posuere id, congue at mauris. Nunc tempor elit dolor, vel placerat dolor mattis nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus diam, sollicitudin a felis vitae, mollis congue mi. Ut in euismod dolor. Mauris placerat egestas tortor quis iaculis. Pellentesque et felis tortor. Mauris massa metus, pellentesque eu orci vulputate, dignissim imperdiet dolor.
 Donec bibendum felis quis tellus condimentum, id laoreet orci tempor. Quisque non fermentum risus. Praesent at orci quis felis volutpat condimentum. Morbi sapien dolor, dapibus quis consectetur sit amet, gravida vel orci. Duis nulla mi, placerat vel convallis quis, vestibulum ac nunc. Duis ac sollicitudin purus. Phasellus ullamcorper erat at ante congue ullamcorper at in mauris. Suspendisse quis metus placerat, condimentum sapien eget, elementum justo. Vestibulum tincidunt tempus nisl, at sodales mi varius nec. Donec quis est laoreet, placerat orci eget, tincidunt erat. Nullam pretium gravida neque eget porttitor. In hac habitasse platea dictumst. Integer semper sit amet felis eu feugiat.
 Curabitur lobortis, sem at cursus cursus, felis lacus venenatis sapien, euismod pharetra diam dolor sed dolor. Donec dignissim, augue ac ultrices aliquam, metus diam semper ante, id venenatis purus lectus at diam. Nulla a posuere nunc, non aliquet libero. Quisque magna nisi, pulvinar a facilisis pharetra, suscipit sed nisi. Ut porttitor lorem eu eros scelerisque rutrum. Sed iaculis sapien eu elit rutrum venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.)***";
@@ -23,6 +27,9 @@ typedef chrono::steady_clock hr_clock;
 typedef hr_clock::time_point hr_time;
 typedef chrono::milliseconds hr_prec;
 
+/******************************************************************************
+ * STL Map Benchmark
+******************************************************************************/
 void nativeBench() {
     std::cout.sync_with_stdio( false );
     std::cout << "Running Native benchmark." << std::endl;
@@ -47,7 +54,9 @@ void nativeBench() {
             << '\t' << testTree.size() << "\n\n";
 }
 
-
+/******************************************************************************
+ * B-Tree Benchmark
+******************************************************************************/
 void bTreeBench() {
     std::cout.sync_with_stdio( false );
     std::cout << "Running bTree benchmark." << std::endl;
@@ -72,22 +81,98 @@ void bTreeBench() {
             << '\t' << testTree.size() << "\n\n";
 }
 
+/******************************************************************************
+ * Test Copy function to return an initialized B-Tree
+******************************************************************************/
+hamLibs::containers::bTree< const char*, int > getTempTree() {
+    hamLibs::containers::bTree< const char*, int > tempTree;
+    
+    tempTree[ testKey1 ] = 0;
+    tempTree[ testKey2 ] = 1;
+    tempTree[ testKey3 ] = 2;
+    tempTree[ loremIpsum ] = 3;
+    
+    return tempTree;
+}
+
+/******************************************************************************
+ * B-Tree Copy Constructor Test
+******************************************************************************/
+void testCopyCtor() {
+    std::cout << "\nTESTING COPY C-TOR" << "\n";
+    
+    hamLibs::containers::bTree< const char*, int > otherTree( getTempTree() );
+    
+    std::cout << otherTree[testKey1] << "\n";
+    std::cout << otherTree[testKey2] << "\n";
+    std::cout << otherTree[testKey3] << "\n";
+    std::cout << otherTree[loremIpsum] << "\n";
+    
+    std::cout << "B-Tree Size: " << otherTree.size() << "\n";
+}
+
+/******************************************************************************
+ * B-Tree Copy Operator Test
+******************************************************************************/
+void testCopyOp() {
+    std::cout << "\nTESTING COPY OPERATOR" << "\n";
+    
+    hamLibs::containers::bTree< const char*, int > otherTree;
+    otherTree = getTempTree();
+    
+    std::cout << otherTree[testKey1] << "\n";
+    std::cout << otherTree[testKey2] << "\n";
+    std::cout << otherTree[testKey3] << "\n";
+    std::cout << otherTree[loremIpsum] << "\n";
+    
+    std::cout << "B-Tree Size: " << otherTree.size() << "\n";
+}
+
+/******************************************************************************
+ * B-Tree Move Constructor Test
+******************************************************************************/
+void testMoveCtor() {
+    std::cout << "\nTESTING MOVE CONSTRUCTOR" << "\n";
+    
+    hamLibs::containers::bTree< const char*, int > otherTree(std::move(getTempTree()));
+    
+    std::cout << otherTree[testKey1] << "\n";
+    std::cout << otherTree[testKey2] << "\n";
+    std::cout << otherTree[testKey3] << "\n";
+    std::cout << otherTree[loremIpsum] << "\n";
+    
+    std::cout << "B-Tree Size: " << otherTree.size() << "\n";
+}
+
+/******************************************************************************
+ * B-Tree Move Operator Test
+******************************************************************************/
+void testMoveOp() {
+    std::cout << "\nTESTING MOVE OPERATOR" << "\n";
+    
+    hamLibs::containers::bTree< const char*, int > otherTree;
+    otherTree = std::move(getTempTree());
+    
+    std::cout << otherTree[testKey1] << "\n";
+    std::cout << otherTree[testKey2] << "\n";
+    std::cout << otherTree[testKey3] << "\n";
+    std::cout << otherTree[loremIpsum] << "\n";
+    
+    std::cout << "B-Tree Size: " << otherTree.size() << "\n";
+}
+
+/******************************************************************************
+ * Main
+******************************************************************************/
 int main() {
-    
     // Benchmarks
-    nativeBench();
-    bTreeBench();
+    //nativeBench();
+    //bTreeBench();
     
-    hamLibs::containers::bTree< const char*, int > strTree;
-    strTree[ "Hello World!" ] = 0;
-    strTree[ "Supercalifragilisticexpialidocious!" ] = 1;
-    strTree[ "One, Two, Three" ] = 2;
-    //strTree[ loremIpsum ] = 3;
-    
-    std::cout << strTree["Hello World!"] << "\n";
-    std::cout << strTree["Supercalifragilisticexpialidocious!"] << "\n";
-    std::cout << strTree["One, Two, Three"] << "\n";
-    //std::cout << strTree[loremIpsum];
+    testCopyCtor();
+    testCopyOp();
+    testMoveCtor();
+    testMoveOp();
     
     return std::cin.get();
 }

14253ca4e2d8ab9bdb810db9253c843f655c8f01
hamsham <mileslacey@gmail.com>
2014-03-29 12:39:41 -0700

    Reimplemented compile-time endian checking for little, big, and PDP endian-based machines.

--- include/defs/endian.h
+++ include/defs/endian.h
@@ -14,8 +14,6 @@ namespace hamLibs {
 namespace utils {
 
 /**
- * hl_endianness
- *
  * This enumeration can be placed into templated objects in order to generate
  * compile-time code based on a program's target endianness.
  *
@@ -23,39 +21,35 @@ namespace utils {
  * order to manually compare them against the number order in the
  * endianValues[] array.
  */
-enum hl_endianness : uint32_t {
-    HL_LITTLE_ENDIAN   = 0x03020100,
-    HL_BIG_ENDIAN      = 0x00010203,
-    HL_PDP_ENDIAN      = 0x01000302,
+enum endian_t : uint32_t {
+    HL_LITTLE_ENDIAN   = 0x00000001,
+    HL_BIG_ENDIAN      = 0x01000000,
+    HL_PDP_ENDIAN      = 0x00010000,
     HL_UNKNOWN_ENDIAN  = 0xFFFFFFFF
 };
-
-/**
- * A constant array used to determine a program's target endianness. The
- * values
- *  in this array can be compared against the values placed in the
- * hl_endianness enumeration.
- */
-static constexpr uint8_t endianValues[4] = {0, 1, 2, 3};
 
 /**
  * A simple function that can be used to help determine a program's endianness
  * at compile-time.
  */
-constexpr hl_endianness getEndianOrder() {
+constexpr endian_t getEndianOrder() {
     return
-        (0x00 == endianValues[0])           // If Little Endian Byte Order,
-            ? HL_LITTLE_ENDIAN              // return 0 for little endian.
-            : (0x03 == endianValues[0])     // Else if Big Endian Byte Order,
-                ? HL_BIG_ENDIAN             // return 1 for big endian.
-                : (0x02 == endianValues[0]) // Else if PDP Endian Byte Order,
-                    ? HL_PDP_ENDIAN         // return 2 for pdp endian.
-                    : HL_UNKNOWN_ENDIAN;    // Else return -1 for wtf endian.
+        ((1 & 0xFFFFFFFF) == HL_LITTLE_ENDIAN)
+            ? HL_LITTLE_ENDIAN
+            : ((1 & 0xFFFFFFFF) == HL_BIG_ENDIAN)
+                ? HL_BIG_ENDIAN
+                : ((1 & 0xFFFFFFFF) == HL_PDP_ENDIAN)
+                    ? HL_PDP_ENDIAN
+                    : HL_UNKNOWN_ENDIAN;
 }
 
-} /* End Utils namespace */
-} /* End HamLibs namespace */
+} /* end utils namespace */
+} /* End hamLibs namespace */
 
+/**
+ * Macro that should be used to always get the endianness of the current build
+ * target.
+ */
 #define HL_ENDIANNESS hamLibs::utils::getEndianOrder()
 
 #endif /* __HL_ENDIAN_H__ */

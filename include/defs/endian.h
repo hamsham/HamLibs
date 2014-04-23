7a581206b1938069771b092fd217fb36d24191aa
hamsham <mileslacey@gmail.com>
2014-04-22 22:50:09 -0700

    Added helper functions to swap the byte order of various plain old data types. Types with a fixed byte-size may be used at compile-time.

--- include/defs/endian.h
+++ include/defs/endian.h
@@ -43,6 +43,131 @@ constexpr endian_t getEndianOrder() {
                     : HL_UNKNOWN_ENDIAN;
 }
 
+/**
+ * Swap the bytes of an unsigned 16-bit integral type between big and little
+ * endian representation. This function can be used at compile-time.
+ * 
+ * @param n
+ * an unsigned integral type.
+ * 
+ * @return uint16_t
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+constexpr uint16_t btol(uint16_t n) {
+    return (n >> 8) ^ (n << 8);
+}
+
+/**
+ * Swap the bytes of a signed 16-bit integral type between big and little
+ * endian representation. This function can be used at compile-time.
+ * 
+ * @param n
+ * a signed integral type.
+ * 
+ * @return int16_t
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+constexpr int16_t btol(int16_t n) {
+    return (int16_t)btol((uint16_t)n);
+}
+
+/**
+ * Swap the bytes of an unsigned 32-bit integral type between big and little
+ * endian representation. This function can be used at compile-time.
+ * 
+ * @param n
+ * an unsigned integral type.
+ * 
+ * @return uint32_t
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+constexpr uint32_t btol(uint32_t n) {
+    return
+    (0x000000FF & (n >> 24)) ^
+    (0x0000FF00 & (n >> 8)) ^
+    (0x00FF0000 & (n << 8)) ^
+    (0xFF000000 & (n << 24));
+}
+
+/**
+ * Swap the bytes of a signed 32-bit integral type between big and little
+ * endian representation. This function can be used at compile-time.
+ * 
+ * @param n
+ * a signed integral type.
+ * 
+ * @return int32_t
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+constexpr int32_t btol(int32_t n) {
+    return (int32_t)btol((uint32_t)n);
+}
+
+/**
+ * Swap the bytes of an unsigned 64-bit integral type between big and little
+ * endian representation. This function can be used at compile-time.
+ * 
+ * @param n
+ * an unsigned integral type.
+ * 
+ * @return uint64_t
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+constexpr uint64_t btol(uint64_t n) {
+    return
+    (0x00000000000000FF & (n >> 56)) ^
+    (0x000000000000FF00 & (n >> 40)) ^
+    (0x0000000000FF0000 & (n >> 24)) ^
+    (0x00000000FF000000 & (n >> 8)) ^
+    (0x000000FF00000000 & (n << 8)) ^
+    (0x0000FF0000000000 & (n << 24)) ^
+    (0x00FF000000000000 & (n << 40)) ^
+    (0xFF00000000000000 & (n << 56));
+}
+
+/**
+ * Swap the bytes of a signed 64-bit integral type between big and little
+ * endian representation. This function can be used at compile-time.
+ * 
+ * @param n
+ * a signed integral type.
+ * 
+ * @return int64_t
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+constexpr int64_t btol(int64_t n) {
+    return (int64_t)btol((uint64_t)n);
+}
+
+/**
+ * Swap the bytes of a POD type between big and little endian representation.
+ * 
+ * @param n
+ * A plain-old-data type.
+ * 
+ * @return numType
+ * The value of the input parameter with its bytes swapped between big & little
+ * endian representation.
+ */
+template <typename numType>
+numType btol(numType n) {
+    numType ret;
+    unsigned char* const pIn = (unsigned char* const)&n;
+    unsigned char* const pOut = (unsigned char* const)&ret;
+    
+    for (unsigned i = 0, j = sizeof(numType)-1; i < sizeof(numType); ++i, --j) {
+        pOut[i] = pIn[j];
+    }
+    
+    return ret;
+}
+
 } /* end utils namespace */
 } /* End hamLibs namespace */
 
